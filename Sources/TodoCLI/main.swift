b3b68ae75d285fdb451110067c46cd6be8fdac36
jaroshevskii <jaroshevskiisasha@gmail.com>
2022-09-25 19:55:25 +0300

    Update enum Color

--- Sources/TodoCLI/main.swift
+++ Sources/TodoCLI/main.swift
@@ -1,117 +1,85 @@
-// void printVersion() {
-//   std::cout << "todo-cli version 0.1.0\n\n";
-// }
-func printVersion() {
-    print("todo-cli version 0.1.0\n")
+enum Color: String {
+    static let reset = "\u{001B}[0m"
+    static let black = "\u{001B}[0;30m"
+    static let red = "\u{001B}[0;31m"
+    static let green = "\u{001B}[0;32m"
+    static let yellow = "\u{001B}[0;33m"
+    static let blue = "\u{001B}[0;34m"
+    static let purple = "\u{001B}[0;35m"
+    static let cyan = "\u{001B}[0;36m"
+    static let white = "\u{001B}[0;37m"
 }
 
-// void printHelp() {
-//   // Print description.
-//   std::cout << "Create and manage tasks list from the command line.\n\n";
-//
-//   // Print usage.
-//   std::cout << "Usage:\n"
-//             << "  /path/to/app/todo-cli <command>\n\n";
-//
-//   // Print commands.
-//   std::cout << "Commands:\n"
-//             << "  version  Print version.\n"
-//             << "  help     Print help.\n"
-//             << "  test     Print argc and argv.\n\n";
-//
-//   // Print examples:
-//   std::cout << "Examples:\n"
-//             << "  $ ./todo-cli\n"
-//             << "  $ ./todo-cli version\n"
-//             << "  $ ./todo-cli help\n\n";
-// }
-func printHelp() {
+func printLogo() {
+    let logoColorMain = Color.green
+    let logoColorSecondary = Color.purple
     print("""
+        \(logoColorMain) ______        __     \(logoColorSecondary)_______   ____
+        \(logoColorMain)/_  __/__  ___/ /__  \(logoColorSecondary)/ ___/ /  /  _/
+        \(logoColorMain) / / / _ \\/ _  / _ \\\(logoColorSecondary)/ /__/ /___/ /
+        \(logoColorMain)/_/  \\___/\\_,_/\\___/\(logoColorSecondary)\\___/____/___/
+    \(Color.reset.rawValue)
+    """)
+}
+
+func printVersion() {
+    print("""
+    TodoCLI v0.1.0
     Create and manage tasks list from the command line.
 
-    Usage:
-      /path/to/app/dir/todo-cli <command>
+    """)
+}
 
-    Commands:
-      version  Print version.
-      help     Print help.
-      test     Print argc and argv.
+func printHelp() {
+    print("""
+    \(Color.yellow)Usage:\(Color.reset)
+      /path/to/app/dir/TodoCLI <command>
+
+    \(Color.yellow)Commands:\(Color.reset)
+      version  Prints version.
+      help     Prints help.
+      test     Prints arguments.
     
-    Examples:
-      $ ./todo-cli
-      $ ./todo-cli version
-      $ ./todo-cli help
+    \(Color.yellow)Examples:\(Color.reset)
+      $ TodoCLI
+      $ TodoCLI version
+      $ TodoCLI help
 
     """)
 }
 
-// void test(int argc, char *argv[]) {
-//   std::cout << "argc:  " << argc << '\n'
-//             << "argv:\n";
-
-//   for (int argIndex = 0; argIndex < argc; ++argIndex) {
-//     std::cout << "  " << argv[argIndex] << '\n';
-//   }
-//   std::cout << '\n';
-// }
-func test(_ arguments: [String]) {
+func test(arguments: [String]) {
     print("""
     Arguments count: \(arguments.count)
-    Arguments:
+    Arguments:       \(arguments)
+    
     """)
-
-    for argument in arguments {
-        print("  \(argument)")
-    }
-
-    print()  // Prints empty line.
 }
 
-// int main(int argc, char *argv[]) {
-//   if (argc == 1) {
-//     printVersion();
-//     printHelp();
-//   }
-// 
-//   int unknownArgumetsCount = 0;
-// 
-//   for (int argIndex = 1; argIndex < argc; ++argIndex) {
-//     const auto &argument = std::string(argv[argIndex]);
-// 
-//     if (argument == "version") {
-//       printVersion();
-//     } else if (argument == "help") {
-//       printHelp();
-//     } else if (argument == "test") {
-//       test(argc, argv);
-//     } else {
-//       ++unknownArgumetsCount;
-//       std::cerr << "Error: Ohh shit. You used an unknown argument `" << argument << "`.\n\n";
-//     }
-//   }
-//   return 0;
-// }
+// MARK: Main
+
 var arguments = CommandLine.arguments
 
 if arguments.count == 1 {
+    printLogo()
     printVersion()
     printHelp()
 } else {
-    arguments.removeFirst()  // Removes app path.
+    // Removes app path.
+    arguments.removeFirst()
 
-    var unknownArgumetsCount = 0
+    switch arguments.first {
+    case "version":
+        printVersion()
+    case "help":
+        printHelp()
+    case "test":
+        test(arguments: arguments)
+    default:
+        print("""
+        \(Color.red)Error:\(Color.reset) Ohh shit. You used an unknown argument(s): \(arguments).
+        \(Color.blue)Note:\(Color.reset)  Use `$ TodoCLI help` to see the list of available commands.
 
-    for argument in arguments {
-        switch argument {
-        case "version":
-            printVersion()
-        case "help":
-            printHelp()
-        case "test":
-            test(arguments)
-        default:
-            print("Error: Ohh shit. You used an unknown argument '\(argument)'.\n")
-            unknownArgumetsCount += 1
-        }
+        """)
     }
 }
